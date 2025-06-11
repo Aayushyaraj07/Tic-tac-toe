529a5eeee74594cebdcc88ad8b15866462546822
Aayushyaraj07 <aayushmohite07@gmail.com>
2025-06-12 01:37:21 +0530

    second day

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div>
-      <h1 className='text-center text-3xl'>Hello React</h1>
+      <div className="grid grid-rows-3 p-10 mt-60 m-auto w-[30%] rounded-xl bg-fuchsia-500">
+        <div className="grid grid-cols-3">
+          <div className="p-10 text-center bg-white m-3 rounded-xl">1</div>
+          <div className="p-10 text-center bg-white m-3 rounded-xl">2</div>
+          <div className="p-10 text-center bg-white m-3 rounded-xl">3</div>
+        </div>
+        <div className="grid grid-cols-3">
+          <div className="p-10 text-center bg-white m-3 rounded-xl">1</div>
+          <div className="p-10 text-center bg-white m-3 rounded-xl">2</div>
+          <div className="p-10 text-center bg-white m-3 rounded-xl">3</div>
+        </div>
+        <div className="grid grid-cols-3">
+          <div className="p-10 text-center bg-white m-3 rounded-xl">1</div>
+          <div className="p-10 text-center bg-white m-3 rounded-xl">2</div>
+          <div className="p-10 text-center bg-white m-3 rounded-xl">3</div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
